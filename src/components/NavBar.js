d0ff8dc29d5b027cc3800f997d49b2c2412556c4
kieranjoyce <kieran.joyce99@gmail.com>
2022-06-15 12:04:25 +0100

    add links to category pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom'
 import styles from '../modules/NavBar.module.css'
 
 export default function NavBar({ categories }) {
     return (
         <nav>
             <ul className={styles.nav__list}>
-                {categories.map((category) => {
-                    return <li key={category.slug}
+                {categories.map(({slug}) => {
+                    return <li key={slug}
                         className={styles.nav__listItem}>
-                        {category.slug}
+                        <Link to={`/reviews/${slug}`}>
+                            {slug.split('-').join(' ')}
+                        </Link>
                         </li>
                 })}
             </ul>
