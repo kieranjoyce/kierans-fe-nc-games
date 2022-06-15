1a82c9a1ab8d472e7ff49a8ffcbf2bd4198e2fc0
kieranjoyce <kieran.joyce99@gmail.com>
2022-06-15 16:22:50 +0100

    change links to NavLinks with active styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,20 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styles from '../modules/NavBar.module.css'
+import { dashesToSpaces } from '../utils/utils'
 
 export default function NavBar({ categories }) {
+
     return (
-        <nav>
+        <nav className={styles.nav}>
             <ul className={styles.nav__list}>
                 {categories.map(({slug}) => {
                     return <li key={slug}
                         className={styles.nav__listItem}>
-                        <Link to={`/reviews/${slug}`}>
-                            {slug.split('-').join(' ')}
-                        </Link>
+                        <NavLink to={`/reviews/${slug}`}
+                            className={(({ isActive }) => isActive ? styles.nav__activeLink : undefined)} >
+                            {/* // {styles["nav__link--active"]}> */}
+                            {dashesToSpaces(slug)}
+                        </NavLink>
                         </li>
                 })}
             </ul>
