2d62e2dd5f4acca721a1b7dfe086ffe77e46f13f
kieranjoyce <kieran.joyce99@gmail.com>
2022-06-17 11:27:40 +0100

    add conditional nav to all reviews

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,29 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import styles from '../modules/NavBar.module.css'
 import { dashesToSpaces } from '../utils/utils'
 
 export default function NavBar({ categories }) {
 
+    const location = useLocation();
+    const atAllReviews = location.pathname === '/';
+    
     return (
         <nav className={styles.nav}>
             <ul className={styles.nav__list}>
-                {categories.map(({slug}) => {
-                    return <li key={slug}
-                        className={styles.nav__listItem}>
-                        <NavLink to={`/${slug}`}
-                            className={(({ isActive }) => isActive ? styles.nav__activeLink : undefined)} >
-                            {dashesToSpaces(slug)}
-                        </NavLink>
+                {atAllReviews ? null : <NavLink to={`/`}
+                    className={(({ isActive }) => isActive ? styles.nav__activeLink : undefined)} >
+                        <li key="home" className={styles.nav__listItem} >
+                            Back to all reviews
                         </li>
+                    </NavLink>}
+                {categories.map(({slug}) => {
+                    return <NavLink to={`/${slug}`} key={slug}
+                    className={(({ isActive }) => isActive ? styles.nav__activeLink : undefined)} >
+                        <li
+                        className={styles.nav__listItem}>
+                            {dashesToSpaces(slug)}
+                        </li>
+                    </NavLink>
                 })}
             </ul>
         </nav>
