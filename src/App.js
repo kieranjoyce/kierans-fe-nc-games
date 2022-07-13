9af47ec5df6b034770f498f2023322cd88040ea1
kieranjoyce <kieran.joyce99@gmail.com>
2022-07-13 16:14:18 +0100

    provide user context and fix formatting

--- src/App.js
+++ src/App.js
@@ -5,29 +5,44 @@ import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import ReviewsList from "./components/ReviewsList";
 import SingleReview from "./components/SingleReview";
-import { getCategories} from "./utils/api";
+import { UserContext } from "./contexts/UserContext";
+import { getCategories } from "./utils/api";
 
 function App() {
     const [categories, setCategories] = useState([]);
-    
+
+    const user = {
+        username: "happyamy2016",
+        name: "Amy Happy",
+        avatar_url:
+            "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729",
+    };
+
     useEffect(() => {
-        getCategories()
-        .then(categories => {
+        getCategories().then((categories) => {
             setCategories(categories);
-        })
-    }, [])
+        });
+    }, []);
 
     return (
-        <div className="App">
-        <Header />
-        <NavBar categories={categories}/>
-        <Routes>
-            <Route path="/" element={<ReviewsList />} />
-            <Route path="/:category" element={<ReviewsList categories={categories}/>} />
-            <Route path="/review/:review_id" element={<SingleReview />} />
-        </Routes>
-        </div>
-        );
-    }
-    
+        <UserContext.Provider value={user}>
+            <div className="App">
+                <Header />
+                <NavBar categories={categories} />
+                <Routes>
+                    <Route path="/" element={<ReviewsList />} />
+                    <Route
+                        path="/:category"
+                        element={<ReviewsList categories={categories} />}
+                    />
+                    <Route
+                        path="/review/:review_id"
+                        element={<SingleReview />}
+                    />
+                </Routes>
+            </div>
+        </UserContext.Provider>
+    );
+}
+
 export default App;
