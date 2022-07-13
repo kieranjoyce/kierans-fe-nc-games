3ec0f3a3c1e75eb10fa508b69816e221e133920c
kieranjoyce <kieran.joyce99@gmail.com>
2022-07-13 17:20:49 +0100

    create postComment function and fix formatting

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,43 +1,44 @@
-import axios from 'axios';
+import axios from "axios";
 
 const ncGamesApi = axios.create({
-    baseURL: 'https://kierans-be-nc-games.herokuapp.com/api/'
-})
+    baseURL: "https://kierans-be-nc-games.herokuapp.com/api/",
+});
 
 export function getCategories() {
-    return ncGamesApi.get('/categories')
-    .then(({data: {categories}}) => {
+    return ncGamesApi.get("/categories").then(({ data: { categories } }) => {
         return categories;
-    })
+    });
 }
 
 export function getReviews(category) {
-    return ncGamesApi.get('/reviews', {params: {category}})
-    .then(({ data: {reviews}}) => {
-        return reviews;
-    })
+    return ncGamesApi
+        .get("/reviews", { params: { category } })
+        .then(({ data: { reviews } }) => {
+            return reviews;
+        });
 }
 
 export function getReview(review_id) {
-    return ncGamesApi.get(`/reviews/${review_id}`)
-    .then(({ data: {review}}) => {
-        return review;
-    })
+    return ncGamesApi
+        .get(`/reviews/${review_id}`)
+        .then(({ data: { review } }) => {
+            return review;
+        });
 }
 
 export function getUser(targetUsername) {
-    return ncGamesApi.get('/users')
-    .then(({data : {users}}) => {
-        return users.find(({ username }) => username === targetUsername);
-    })
-    .catch(err => console.log(err))
+    return ncGamesApi
+        .get("/users")
+        .then(({ data: { users } }) => {
+            return users.find(({ username }) => username === targetUsername);
+        })
+        .catch((err) => console.log(err));
 }
 
 export function getUsers() {
-    return ncGamesApi.get('/users')
-    .then(({ data: {users}}) => {
+    return ncGamesApi.get("/users").then(({ data: { users } }) => {
         return users;
-    })
+    });
 }
 
 export function patchVotes(review_id, inc_votes) {
@@ -45,8 +46,20 @@ export function patchVotes(review_id, inc_votes) {
 }
 
 export function getComments(review_id) {
-    return ncGamesApi.get(`/reviews/${review_id}/comments`)
-    .then(({ data: {comments}}) => {
-        return comments;
-    })
+    return ncGamesApi
+        .get(`/reviews/${review_id}/comments`)
+        .then(({ data: { comments } }) => {
+            return comments;
+        });
+}
+
+export function postComment(review_id, username, body) {
+    return ncGamesApi
+        .post(`/reviews/${review_id}/comments`, {
+            username,
+            body,
+        })
+        .then(({ data: { comment } }) => {
+            return comment;
+        });
 }
