671520240b6c08fa1f9d5968673d3e210eb71f94
kieranjoyce <kieran.joyce99@gmail.com>
2022-07-18 14:20:58 +0100

    update link path and fix formatting

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,25 +1,43 @@
 import { Link } from "react-router-dom";
-import styles from '../modules/ReviewCard.module.css'
+import styles from "../modules/ReviewCard.module.css";
 import { dashesToSpaces, formatDate } from "../utils/utils";
 
 export default function ReviewCard({ review }) {
-    const {review_id, title, designer, owner, review_img_url, review_body, category, created_at, votes, comment_count} = review;
-    
+    const {
+        review_id,
+        title,
+        designer,
+        owner,
+        review_img_url,
+        review_body,
+        category,
+        created_at,
+        votes,
+        comment_count,
+    } = review;
+
     return (
         <li className={styles.review}>
-            <Link to={`/review/${review_id}`} >
+            <Link to={`/reviews/${review_id}`}>
                 <h3 className={styles.title}>{title}</h3>
                 <div className={styles.detailsWrapper}>
-                    <img src={review_img_url} alt={title} 
-                    className={styles.image} />
+                    <img
+                        src={review_img_url}
+                        alt={title}
+                        className={styles.image}
+                    />
                     <div className={styles.reviewDetails}>
                         <p className={styles.owner}>{owner}</p>
                         <p className={styles.date}>{formatDate(created_at)}</p>
                     </div>
                 </div>
                 <div className={styles.gameDetails}>
-                    <p className={styles.category}>{dashesToSpaces(category)}</p>
-                    <p>designed by<span>{designer}</span></p>
+                    <p className={styles.category}>
+                        {dashesToSpaces(category)}
+                    </p>
+                    <p>
+                        designed by<span>{designer}</span>
+                    </p>
                 </div>
                 <p className={styles.body}>{review_body}</p>
                 <div className={styles.engagement}>
@@ -28,6 +46,5 @@ export default function ReviewCard({ review }) {
                 </div>
             </Link>
         </li>
-        
-    )
+    );
 }
