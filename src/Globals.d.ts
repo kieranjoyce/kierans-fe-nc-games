a87a626dcf5df5e17c879dfea339ec4b2a5dc884
kieranjoyce <kieran.joyce99@gmail.com>
2022-09-04 18:49:55 +0100

    add type declaration for svg files

--- src/Globals.d.ts
+++ src/Globals.d.ts
@@ -1,1 +1,6 @@
 declare module "*.module.css";
+declare module "*.svg" {
+    export const ReactComponent: JSX.IntrinsicElements.svg;
+    const src: string; // React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+    export default src;
+}
