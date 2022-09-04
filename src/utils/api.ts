5a9623b68bb77a31599543ee316e4b9530c37d7a
kieranjoyce <kieran.joyce99@gmail.com>
2022-09-04 17:27:47 +0100

    move interface declarations to new file

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,41 +1,9 @@
 import axios from "axios";
+import type { Category, Review, User, Comment } from "../types";
 
 const ncGamesApi = axios.create({
     baseURL: "https://kierans-be-nc-games.herokuapp.com/api/",
 });
-
-export interface Category {
-    slug: string;
-    description: string;
-}
-
-export interface Review {
-    review_id: number;
-    title: string;
-    designer: string;
-    owner: string;
-    review_img_url: string;
-    review_body: string;
-    category: string;
-    created_at: string;
-    votes: number;
-    comment_count: number;
-}
-
-export interface User {
-    username: string;
-    name: string;
-    avatar_url: string;
-}
-
-export interface Comment {
-    comment_id: number;
-    body: string;
-    review_id: number;
-    author: string;
-    votes: number;
-    created_at: string;
-}
 
 export function getCategories() {
     return ncGamesApi
