99e046618a51792d536245bb9293e899e22e1908
kieranjoyce <kieran.joyce99@gmail.com>
2022-12-04 21:08:53 +0000

    convert review image and details to mui

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -2,6 +2,15 @@ import { Link } from "react-router-dom";
 import styles from "../modules/ReviewCard.module.css";
 import { dashesToSpaces, formatDate } from "../utils/utils";
 import type { Review } from "../types";
+import {
+    Box,
+    Card,
+    CardActionArea,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    Typography,
+} from "@mui/material";
 
 interface ReviewCardProps {
     review: Review;
@@ -22,32 +31,46 @@ export default function ReviewCard({ review }: ReviewCardProps) {
     } = review;
 
     return (
-        <li className={styles.review}>
-            <Link to={`/reviews/${review_id}`}>
-                <h3 className={styles.title}>{title}</h3>
-                <div className={styles.detailsWrapper}>
-                    <img
-                        src={review_img_url}
-                        alt={title}
-                        className={styles.image}
-                    />
-                    <div className={styles.reviewDetails}>
-                        <p className={styles.owner}>{owner}</p>
-                        <p className={styles.date}>{formatDate(created_at)}</p>
-                    </div>
-                </div>
-                <div className={styles.gameDetails}>
-                    <p>{dashesToSpaces(category)}</p>
-                    <p>
-                        designed by<span>{designer}</span>
-                    </p>
-                </div>
-                <p className={styles.body}>{review_body}</p>
-                <div className={styles.engagement}>
-                    <p>votes: {votes}</p>
-                    <p>comments: {comment_count}</p>
-                </div>
-            </Link>
-        </li>
+        <Card>
+            <CardActionArea component={Link} to={`/reviews/${review_id}`}>
+                <CardHeader title={title} subheader={`By ${owner}`} />
+                <CardMedia component="img" image={review_img_url} alt={title} />
+                <CardContent>
+                    <Typography variant="body2">
+                        {formatDate(created_at)}
+                    </Typography>
+                    <Typography variant="body1">
+                        {dashesToSpaces(category)}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+        // <li className={styles.review}>
+        //     <Link to={`/reviews/${review_id}`}>
+        //         <h3 className={styles.title}>{title}</h3>
+        //         <div className={styles.detailsWrapper}>
+        //             <img
+        //                 src={review_img_url}
+        //                 alt={title}
+        //                 className={styles.image}
+        //             />
+        //             <div className={styles.reviewDetails}>
+        //                 <p className={styles.owner}>{owner}</p>
+        //                 <p className={styles.date}>{formatDate(created_at)}</p>
+        //             </div>
+        //         </div>
+        //         <div className={styles.gameDetails}>
+        //             <p>{dashesToSpaces(category)}</p>
+        //             <p>
+        //                 designed by<span>{designer}</span>
+        //             </p>
+        //         </div>
+        //         <p className={styles.body}>{review_body}</p>
+        //         <div className={styles.engagement}>
+        //             <p>votes: {votes}</p>
+        //             <p>comments: {comment_count}</p>
+        //         </div>
+        //     </Link>
+        // </li>
     );
 }
