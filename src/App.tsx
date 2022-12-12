3c8521f41123ae7dc5ddadc9ebaaf570d0aa98b4
kieranjoyce <kieran.joyce99@gmail.com>
2022-12-12 15:28:07 +0000

    remove old styling including NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,6 @@ function App() {
         <UserContext.Provider value={user}>
             <div className="App">
                 <Header />
-                <NavBar categories={categories} />
                 <Routes>
                     <Route path="/" element={<ReviewsList />} />
                     <Route
