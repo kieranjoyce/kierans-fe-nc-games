88984be72f00d6d769514cbacf42fb00b5ddf160
kieranjoyce <kieran.joyce99@gmail.com>
2022-12-12 15:28:28 +0000

    begin restyling header to mui AppBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import styles from "../modules/Header.module.css";
 
 export default function Header() {
     return (
-        <header className={styles.header}>
-            <h1 className={styles.title}>
+        <header>
+            <AppBar position="sticky" sx={{ mb: "1rem", py: "0.5rem" }}>
+                <Toolbar>
+                    <Typography variant="h2" component="h1">
+                        <Link to="/">Yellow Pieces logoified pls</Link>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            {/* <h1 className={styles.title}>
                 <Link to="/">Yellow Pieces</Link>
             </h1>
-            <p>login</p>
+            <p>login</p> */}
         </header>
     );
 }
